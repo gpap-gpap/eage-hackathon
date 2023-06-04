8293e475be267418168fdf9d70cee15644e5d6c9
giorgos papageorgiou <gpap.gpap@gmail.com>
2023-06-05 00:18:17 +0200

    replcad

--- public.py
+++ public.py
@@ -119,9 +119,9 @@ with col2:
         wf = qs.word_frequency(cont).most_common(6)
         query, readable = qs.create_query(cont)
         st.write(readable)
-        if st.button("Search"):
-            # new_data = qs.return_first_page(query=query)
-            # st.dataframe(new_data)
+        # if st.button("Search"):
+        # new_data = qs.return_first_page(query=query)
+        # st.dataframe(new_data)
 # string = ""
 # for i in indices:.replace(" ", "+")
 # chunks = dictionary_out[0]["Title"]
@@ -131,4 +131,3 @@ with col2:
 # st.dataframe(new_data)
 
 # def app():
-#
